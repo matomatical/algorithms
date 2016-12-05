b9374152c7699a5880de3106d400b11e800bbf0d
Matt F <matomatical@gmail.com>
2016-12-06 02:26:19 +1100

    added some basic tests for UnionFinder testing properties of the find function

--- test/UnionFinderTest.java
+++ test/UnionFinderTest.java
@@ -1,10 +1,53 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.matomatical.ads.UnionFinder;
+
 public class UnionFinderTest {
 	
 	@Test
-	public void testTestShouldPassTest() {
-		Assert.assertTrue("hello, world!", true);
+	public void findShouldRejectUnconnectedNodes() {
+		UnionFinder uf = new UnionFinder(2);
+		
+		Assert.assertFalse(uf.find(1, 0));
+	}
+
+	@Test
+	public void findShouldAcceptConnectedNodes() {
+		UnionFinder uf = new UnionFinder(2);
+		
+		uf.union(1, 0);
+		
+		Assert.assertTrue(uf.find(1, 0));
+	}
+	
+	@Test
+	public void findShouldAcceptIndirectlyConnectedNodes() {
+		UnionFinder uf = new UnionFinder(3);
+		
+		uf.union(0, 1);
+		uf.union(1, 2);
+
+		Assert.assertTrue(uf.find(0, 2));
+	}
+
+	@Test
+	public void findShouldAcceptSameNodes() {
+		UnionFinder uf = new UnionFinder(2);
+		
+		Assert.assertTrue(uf.find(0, 0));
+	}
+
+	@Test
+	public void findShouldNotDependOnOrderWhenTrue() {
+		UnionFinder uf = new UnionFinder(2);
+		uf.union(1, 0);
+		Assert.assertTrue(uf.find(0, 1) == uf.find(1, 0));
+	}
+
+	@Test
+	public void findShouldNotDependOnOrderWhenFalse() {
+		UnionFinder uf = new UnionFinder(2);
+		Assert.assertTrue(uf.find(0, 1) == uf.find(1, 0));
 	}
 }
