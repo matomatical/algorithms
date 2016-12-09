e88c34827f4a61fae2659910b9a17e8b8fabae38
Matt F <matomatical@gmail.com>
2016-12-09 18:05:31 +1100

    cool bubble sort improvement explanation

--- src/Sorts.java
+++ src/Sorts.java
@@ -44,24 +44,34 @@ public class Sorts {
 		}
 	}
 
+	// i thought of this really cool improvement over the basic bubble sort
+	// implementation.
+	// It's a way to avoid using a 'changed' flag and also skip some unecessary
+	// comparisons while still finishing early.
+	//
+	// While implementing, I wrote a lot of comments to crystalis my thought 
+	// process, and I've left them in so now the method has more comments than
+	// code.
+	// After implementing and testing the method, I looked it up to see if it
+	// has been done before (and of course it has). Wikipedia reports that the 
+	// improvement results in "about a worst case 50% improvement in comparison
+	// count (though no improvement in swap counts)", which makes sense!
 	public static <T extends Comparable<T>> void bubbleSort(T[] A) {
-		// in this experimental implementation of bubble sort we're going to 
-		// try to avoid using a 'changed' flag and also skip some unecessary 
-		// comparisons
-
-		// i will be the length of the section at the beginning of the array
+		
+		// `i` will be the length of the section at the beginning of the array
 		// which may contain out-of-order pairs
 		int i = A.length;
 
 		// we will iterate until this section vanishes (and the array is sorted)
 		while (i > 0) {
 
-			// we will track swaps with k to determine the right value of i for 
-			// the next pass through the array. k begins at zero, and if no
-			// swaps are made, i will become zero and we will finish early
+			// we will track swaps with `k` to determine the right value of `i`
+			// for the next pass through the array. `k` begins at zero, and if 
+			// no swaps are made (or only the first two elements are swappaed)
+			// `i` will become zero and we will finish early!
 			int k = 0;
 
-			// work through all pairs with indices (j-1, j) in the
+			// work through all pairs with indices (`j-1`, `j`) in the
 			// possibly-unordered section
 
 			for (int j = 1; j < i; j++) {
@@ -70,21 +80,23 @@ public class Sorts {
 				if (less(A[j], A[j-1])) {
 					swap(A, j, j-1);
 
-					// but also record the value of j-1 in k, because if no 
+					// but also record the value of `j-1` in `k`, because if no 
 					// more swaps are made during this pass, we can be certain
-					// that everything after index j-1 is already sorted
+					// that everything after index `j-1` is already sorted!
 					k = j-1;
 				}
 			}
 
-			// now, to prepare for the next iteration, take the new value of i
-			// stored in k.
+			// now, to prepare for the next iteration, set the new value of `i`
 			i = k;
-			// the largest this can be is the current value of i minus 1, 
-			// corresponding to a swap between the last two elements (of the first i elements). This is the usual bubble sort decrement i--.
+			
+			// the largest this can be is the current value of `i`, minus 1, 
+			// corresponding to a swap between the last two elements (of the 
+			// first `i` elements). This is the usual bubble sort decrement 
+			// `i--`.
 
-			// eventually, k will be 0 (no swaps made) and we will break out of
-			// the while loop and finish the sorting algorithm.
+			// therefore, eventually `k` will be 0 and we will break out 
+			// of the while loop and finish the sorting algorithm.
 		}
 	}
 }
