bd398ec29484bd4301c53b4b8637d48dc138e3ce
Matt F <matomatical@gmail.com>
2016-12-17 18:27:43 +1100

    BST deletion and lots of tests for it

--- src/BinarySearchTree.java
+++ src/BinarySearchTree.java
@@ -138,4 +138,119 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
 		return false;
 	}
 
+	/**
+	 * Delete the entry associated with a given key from the tree.
+	 * @param key the key whose entry is to be removed from the tree
+	 */
+	public void delete(Key key){
+		root = delete(root, key);
+	}
+
+	/**
+	 * Helper method to recursively find the node to be deleted (based on key)
+	 * and update the tree on the way back
+	 * @param key the key whose entry is to be removed from the tree
+	 * @return the node provided, or its replacement if the tree was modified
+	 * during deletion
+	 * <br>
+	 * usage example:
+	 * `node.left = delete(node.left, key)`
+	 * <br>
+	 * this will search for the key to delete, and update node's left subtree 
+	 * if needs to change during the deletion (for example if it was pointing 
+	 * at the node that was deleted, and now needs to point at the replacement 
+	 * or null)
+	 */
+	private Node<Key, Value> delete(Node<Key, Value> node, Key key) {
+
+		// base case
+		if (node == null) {
+			// if we run into a null node, then we have concluded that the 
+			// node to be deleted is not in the tree.
+			return null;
+		}
+
+		// recursive case
+		int comparison = node.key.compareTo(key);
+		if(comparison < 0) {
+			// node < key, key might be on the right side:
+			node.right = delete(node.right, key);
+
+		} else if (comparison > 0) {
+			// node > key, key might be on the left side:
+			node.left = delete(node.left, key);
+
+		} else {
+			// node == key, this is the key! we found it!
+			// time to actually delete it...
+			return delete(node);
+		}
+
+		// if no change was made, just return the node itself
+		return node;
+	}
+
+	/**
+	 * carry out the actual delete operation on a node in the tree, and return
+	 * the correct replacement node (successor, or null) resulting from removing
+	 * the node from the tree and substituting its successor in its place
+	 * @param node the node to replace
+	 * @return the value that it should be replaced with on the parent
+	 */
+	private Node<Key, Value> delete(Node<Key, Value> node) {
+		if(node.left == null && node.right == null) {
+			// we can just remove this node from the tree, make the parent link
+			// null
+			return null;
+
+		} else if (node.left == null){
+			// left child is null, but right is not. we can just promote the
+			// right child to become the parents new child
+			return node.right;
+
+		} else if (node.right == null) {
+			// right child is null, but left is not. we can just promote the 
+			// left child to become the parents new child
+			return node.left;
+
+		} else {
+			// we have two children, great! lets find and promote our successor
+
+			// find the left-most child of the right child, that's our direct
+			// successor
+			Node<Key, Value> successor = node.right, presuccessor = null;
+			while(successor.left != null) {
+				presuccessor = successor;
+				successor = successor.left;
+			}
+
+			// one thing's for sure, the successor has no left child. it can
+			// take ours!
+			successor.left = node.left;
+
+			// assuming the successor wasn't just the node's right child, we
+			// have to move it up into the node's place in the tree
+			if(successor != node.right) {
+
+				// we need to give the successor's right children to its parent,
+				// in place of the successor itself (child may be null, but 
+				// that's okay too)
+				presuccessor.left = successor.right;
+				
+				// then, we need to give the successor our node's right child
+				// as its own
+				successor.right = node.right;
+			}
+			
+			// if the successor is just our right child, then these 
+			// transformations aren't necessary and all we need to do is 
+			// promote the successor one level up the tree
+
+			// in either case, the final step is to give the successor back to
+			// the parent in place of the original node, which will now be
+			// ready for garbage collection
+
+			return successor;
+		}
+	}
 }
--- test/BinarySearchTreeTest.java
+++ test/BinarySearchTreeTest.java
@@ -248,4 +248,371 @@ public class BinarySearchTreeTest {
 
 		Assert.assertEquals(false, result);
 	}
+
+	@Test
+	public void deleteNodeNotInTreeShouldNotComplain() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+						bst.insert("H", "");
+		bst.insert("D","");				bst.insert("L","");
+							bst.insert("J","");		bst.insert("N","");
+									bst.insert("K","");
+
+		// delete a key that is not in the tree
+		bst.delete("Kalipso");
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "D", "L", "N", "J", "K"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteRootTest1() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+		bst.insert("H", ""); // just a root
+
+		// delete the root
+		bst.delete("H");
+
+		// assert root was deleted
+		Assert.assertFalse(bst.contains("H"));
+	}
+
+	@Test
+	public void deleteRootTest2() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+				bst.insert("H", ""); // root
+		bst.insert("D",""); // and a left child
+
+		// delete the root
+		bst.delete("H");
+
+		// ensure root was deleted
+		Assert.assertFalse(bst.contains("H"));
+
+		// ensure other nodes were not lost
+		Assert.assertTrue(bst.contains("D"));
+	}
+
+	@Test
+	public void deleteRootTest3() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+		bst.insert("H", ""); // root and a right child
+				bst.insert("L","");
+
+		// delete the root
+		bst.delete("H");
+
+		// ensure root was deleted
+		Assert.assertFalse(bst.contains("H"));
+
+		// ensure other nodes were not lost
+		Assert.assertTrue(bst.contains("L"));
+	}
+
+	@Test
+	public void deleteRootTest4() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+					bst.insert("H", ""); // root and both children
+		bst.insert("D","");		bst.insert("L","");
+											bst.insert("N","");
+
+		// delete the root
+		bst.delete("H");
+
+		// ensure root was deleted
+		Assert.assertFalse(bst.contains("H"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"D", "L", "N"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+	
+	@Test
+	public void deleteRootTest5() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+						bst.insert("H", "");
+		bst.insert("D","");				bst.insert("L","");
+							bst.insert("J","");		bst.insert("N","");
+									bst.insert("K","");
+
+		// delete the root
+		bst.delete("H");
+
+		// ensure root was deleted
+		Assert.assertFalse(bst.contains("H"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"D", "L", "N", "J", "K"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteRightNodeTest1() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+						bst.insert("H", "");
+		bst.insert("D","");				bst.insert("L","");
+
+		// delete the right node
+		bst.delete("L");
+
+		// ensure right node was deleted
+		Assert.assertFalse(bst.contains("L"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "D"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteRightNodeTest2() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+						bst.insert("H", "");
+		bst.insert("D","");				bst.insert("L","");
+								bst.insert("J","");
+
+		// delete the right node
+		bst.delete("L");
+
+		// ensure right node was deleted
+		Assert.assertFalse(bst.contains("L"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "D", "J"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteRightNodeTest3() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+						bst.insert("H", "");
+		bst.insert("D","");				bst.insert("L","");
+												bst.insert("N","");
+
+		// delete the right node
+		bst.delete("L");
+
+		// ensure right node was deleted
+		Assert.assertFalse(bst.contains("L"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "D", "N"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+	
+	@Test
+	public void deleteRightNodeTest4() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+						bst.insert("H", "");
+		bst.insert("D","");				bst.insert("L","");
+							bst.insert("J","");		bst.insert("N","");
+
+		// delete the right node
+		bst.delete("L");
+
+		// ensure right node was deleted
+		Assert.assertFalse(bst.contains("L"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "D", "J", "N"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteRightNodeTest5() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+					bst.insert("H", "");
+		bst.insert("D","");			bst.insert("L","");
+						bst.insert("J","");		bst.insert("N","");
+										bst.insert("M","");	bst.insert("O","");
+											bst.insert("Mm","");
+		// delete the right node
+		bst.delete("L");
+
+		// ensure right node was deleted
+		Assert.assertFalse(bst.contains("L"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "D", "J", "N", "M", "O", "Mm"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteRightNodeTest6() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+					bst.insert("H", "");
+		bst.insert("D","");			bst.insert("L","");
+						bst.insert("J","");		bst.insert("N","");
+														bst.insert("O","");
+											
+		// delete the right node
+		bst.delete("L");
+
+		// ensure right node was deleted
+		Assert.assertFalse(bst.contains("L"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "D", "J", "N", "O"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteLeftNodeTest1() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+					bst.insert("H", "");
+		bst.insert("D","");		bst.insert("L","");
+
+		// delete the left node
+		bst.delete("D");
+
+		// ensure left node was deleted
+		Assert.assertFalse(bst.contains("D"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "L"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteLeftNodeTest2() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+								bst.insert("H", "");
+				bst.insert("D","");				bst.insert("L","");
+		bst.insert("B","");
+
+		// delete the left node
+		bst.delete("D");
+
+		// ensure left node was deleted
+		Assert.assertFalse(bst.contains("D"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "L", "B"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteLeftNodeTest3() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+						bst.insert("H", "");
+		bst.insert("D","");				bst.insert("L","");
+				bst.insert("F","");
+
+		// delete the left node
+		bst.delete("D");
+
+		// ensure left node was deleted
+		Assert.assertFalse(bst.contains("D"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "F", "L"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+	
+	@Test
+	public void deleteLeftNodeTest4() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+									bst.insert("H", "");
+					bst.insert("D","");				bst.insert("L","");
+		bst.insert("B","");		bst.insert("F","");
+
+		// delete the left node
+		bst.delete("D");
+
+		// ensure left node was deleted
+		Assert.assertFalse(bst.contains("D"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "L", "B", "F"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteLeftNodeTest5() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+									bst.insert("H", "");
+					bst.insert("D","");				bst.insert("L","");
+		bst.insert("B","");		bst.insert("F","");
+						bst.insert("E","");	bst.insert("G","");
+							bst.insert("Ee","");
+
+		// delete the left node
+		bst.delete("D");
+
+		// ensure left node was deleted
+		Assert.assertFalse(bst.contains("D"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "L", "B", "F", "Ee", "E", "G"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
+
+	@Test
+	public void deleteLeftNodeTest6() {
+		BinarySearchTree<String,String> bst =
+			new BinarySearchTree<String,String>();
+		// create the tree
+									bst.insert("H", "");
+					bst.insert("D","");				bst.insert("L","");
+		bst.insert("B","");		bst.insert("F","");
+										bst.insert("G","");
+											
+		// delete the left node
+		bst.delete("D");
+
+		// ensure left node was deleted
+		Assert.assertFalse(bst.contains("D"));
+
+		// ensure other nodes were not lost
+		for(String key : new String[] {"H", "L", "B", "F", "G", "L"}) {
+			Assert.assertTrue(bst.contains(key));
+		}
+	}
 }
